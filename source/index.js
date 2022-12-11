4d0af8f5b0e30f69dc70944a767e166de6b4d05a
jynxio <1337816495@qq.com>
2022-12-11 23:36:07 +0800

    update

--- source/index.js
+++ source/index.js
@@ -1,7 +1,5 @@
 import "/style/reset.css";
 
-import "/style/index.css";
-
 import * as three from "three";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
@@ -12,16 +10,14 @@ import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 
-import * as dat from "lil-gui";
-
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector("canvas");
+const canvas = document.createElement( "canvas" );
+
+document.body.append( canvas );
 
 // Scene
 const scene = new three.Scene();
@@ -35,7 +31,7 @@ let points;
 
 const parameters = {
     count: 100000,
-    size: 0.01,
+    size: 1,
     radius: 5,
     branches: 3,
     spin: 5,
@@ -45,25 +41,15 @@ const parameters = {
     outsideColor: 0x1b3984,
 };
 
-gui.add(parameters, "count").min(100).max(1000000).step(100).onFinishChange(generateGalaxy);
-gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
-gui.add(parameters, "radius").min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
-gui.add(parameters, "branches").min(2).max(20).step(1).onFinishChange(generateGalaxy);
-gui.add(parameters, "spin").min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
-gui.add(parameters, "randomness").min(0).max(2).step(0.001).onFinishChange(generateGalaxy);
-gui.add(parameters, "randomnessPower").min(1).max(10).step(0.001).onFinishChange(generateGalaxy);
-gui.addColor(parameters, "insideColor").onFinishChange(generateGalaxy);
-gui.addColor(parameters, "outsideColor").onFinishChange(generateGalaxy);
-
-generateGalaxy(parameters);
+generateGalaxy( parameters );
 
 function generateGalaxy() {
 
-    if (points) {
+    if ( points ) {
 
         geometry.dispose();
         material.dispose();
-        scene.remove(points);
+        scene.remove( points );
 
     }
 
@@ -72,57 +58,57 @@ function generateGalaxy() {
      */
     geometry = new three.BufferGeometry();
 
-    const positions = new Float32Array(parameters.count * 3);
-    const colors = new Float32Array(parameters.count * 3);
+    const positions = new Float32Array( parameters.count * 3 );
+    const colors = new Float32Array( parameters.count * 3 );
 
-    const color_inside = new three.Color(parameters.insideColor);
-    const color_outside = new three.Color(parameters.outsideColor);
+    const color_inside = new three.Color( parameters.insideColor );
+    const color_outside = new three.Color( parameters.outsideColor );
 
-    for (let i = 0; i < parameters.count; i++) {
+    for ( let i = 0; i < parameters.count; i ++ ) {
 
         const i_3 = i * 3;
 
         const radius = Math.random() * parameters.radius;
         const spin_angle = radius * parameters.spin;
-        const branch_angle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
+        const branch_angle = ( i % parameters.branches ) / parameters.branches * Math.PI * 2;
 
         //
-        const random_x = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius;
-        const random_y = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius;
-        const random_z = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius;
+        const random_x = Math.pow( Math.random(), parameters.randomnessPower ) * ( Math.random() < 0.5 ? 1 : -1 ) * parameters.randomness * radius;
+        const random_y = Math.pow( Math.random(), parameters.randomnessPower ) * ( Math.random() < 0.5 ? 1 : -1 ) * parameters.randomness * radius;
+        const random_z = Math.pow( Math.random(), parameters.randomnessPower ) * ( Math.random() < 0.5 ? 1 : -1 ) * parameters.randomness * radius;
 
-        positions[i_3] = Math.cos(branch_angle + spin_angle) * radius + random_x;
-        positions[i_3 + 1] = random_y;
-        positions[i_3 + 2] = Math.sin(branch_angle + spin_angle) * radius + random_z;
+        positions[ i_3 ] = Math.cos( branch_angle + spin_angle ) * radius + random_x;
+        positions[ i_3 + 1 ] = random_y;
+        positions[ i_3 + 2 ] = Math.sin( branch_angle + spin_angle ) * radius + random_z;
 
         //
-        const mixed_color = color_inside.clone().lerp(color_outside, radius / parameters.radius);
+        const mixed_color = color_inside.clone().lerp( color_outside, radius / parameters.radius );
 
-        colors[i_3] = mixed_color.r;
-        colors[i_3 + 1] = mixed_color.g;
-        colors[i_3 + 2] = mixed_color.b;
+        colors[ i_3 ] = mixed_color.r;
+        colors[ i_3 + 1 ] = mixed_color.g;
+        colors[ i_3 + 2 ] = mixed_color.b;
 
     }
 
-    geometry.setAttribute("position", new three.BufferAttribute(positions, 3));
-    geometry.setAttribute("color", new three.BufferAttribute(colors, 3));
+    geometry.setAttribute( "position", new three.BufferAttribute( positions, 3 ) );
+    geometry.setAttribute( "color", new three.BufferAttribute( colors, 3 ) );
 
     /**
      * Material
      */
-    material = new three.PointsMaterial({
+    material = new three.PointsMaterial( {
         size: parameters.size,
-        sizeAttenuation: true,
+        sizeAttenuation: false,
         depthWrite: false,
         blending: three.AdditiveBlending,
         vertexColors: true,
-    });
+    } );
 
     /**
      * Points
      */
-    points = new three.Points(geometry, material);
-    scene.add(points);
+    points = new three.Points( geometry, material );
+    scene.add( points );
 
 }
 
@@ -130,20 +116,20 @@ function generateGalaxy() {
  * Size
  */
 const size = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: globalThis.innerWidth,
+    height: globalThis.innerHeight
 };
 
-window.addEventListener("resize", () => {
+globalThis.addEventListener( "resize", () => {
 
-    size.width = window.innerWidth;
-    size.height = window.innerHeight;
+    size.width = globalThis.innerWidth;
+    size.height = globalThis.innerHeight;
 
     camera.aspect = size.width / size.height;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(size.width, size.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.setSize( size.width, size.height );
+    renderer.setPixelRatio( Math.min( globalThis.devicePixelRatio, 2 ) );
 
 })
 
@@ -151,24 +137,25 @@ window.addEventListener("resize", () => {
  * Camera
  */
 // Base camera
-const camera = new three.PerspectiveCamera(75, size.width / size.height, 0.1, 100);
+const camera = new three.PerspectiveCamera( 75, size.width / size.height, 0.1, 100 );
 camera.position.x = 3;
 camera.position.y = 3;
 camera.position.z = 3;
-scene.add(camera);
+scene.add( camera );
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
+const controls = new OrbitControls( camera, canvas );
 controls.enableDamping = true;
 
 /**
  * Renderer
  */
-const renderer = new three.WebGLRenderer({
-    canvas: canvas
-});
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+const renderer = new three.WebGLRenderer( {
+    canvas: canvas,
+    antialias: true
+} );
+renderer.setSize( size.width, size.height );
+renderer.setPixelRatio( Math.min( globalThis.devicePixelRatio, 2 ) );
 
 /**
  * Animate
@@ -179,14 +166,14 @@ tick();
 
 function tick() {
 
-    window.requestAnimationFrame(tick);
+    globalThis.requestAnimationFrame( tick );
 
     const elapsed_time = clock.getElapsedTime();
 
     points.rotation.y = elapsed_time * 0.1;
 
     controls.update();
-    renderer.render(scene, camera);
+    renderer.render( scene, camera );
 
 }
 
@@ -197,29 +184,29 @@ floodlight();
 
 function floodlight() {
 
-    const composer = new EffectComposer(renderer);                                                                    // 效果合成器
-    const pass_render = new RenderPass(scene, camera);                                                                // 后期处理（基本）
-    const pass_bloom = new UnrealBloomPass(new three.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85); // 后期处理（泛光）
+    const composer = new EffectComposer( renderer );                                                                              // 效果合成器
+    const pass_render = new RenderPass( scene, camera );                                                                          // 后期处理（基本）
+    const pass_bloom = new UnrealBloomPass( new three.Vector2( globalThis.innerWidth, globalThis.innerHeight ), 1.5, 0.4, 0.85 ); // 后期处理（泛光）
 
     pass_bloom.renderToScreen = true; // 最终过程是否被渲染到屏幕
     pass_bloom.threshold = 0;         // ？
     pass_bloom.strength = 1;          // 强度
     pass_bloom.radius = 0;            // 半径
 
-    composer.setSize(window.innerWidth, window.innerHeight); // 尺寸
-    composer.addPass(pass_render);                           // 将该后期处理环节添加至过程链
-    composer.addPass(pass_bloom);                            // 将该后期处理环节添加至过程链
+    composer.setSize( globalThis.innerWidth, globalThis.innerHeight ); // 尺寸
+    composer.addPass( pass_render );                                   // 将该后期处理环节添加至过程链
+    composer.addPass( pass_bloom );                                    // 将该后期处理环节添加至过程链
 
-    renderer.setAnimationLoop(() => {
+    renderer.setAnimationLoop( () => {
 
         composer.render();  // 按顺序执行所有启用的后期处理环节, 来产生最终的帧
 
-    });
+    } );
 
-    window.addEventListener("resize", _ => {
+    globalThis.addEventListener( "resize", _ => {
 
-        composer.setSize(window.innerWidth, window.innerHeight);
+        composer.setSize( globalThis.innerWidth, globalThis.innerHeight );
 
-    });
+    } );
 
 }
